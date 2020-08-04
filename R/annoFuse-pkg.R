4af5343094901e60337e35bfbe49dc7383822294
Federico Marini <marinif@uni-mainz.de>
2020-08-05 00:59:47 +0200

    Adding specific file for handling pkg info and imports

---
+++ R/annoFuse-pkg.R
@@ -0,0 +1,26 @@
+#' annoFuse
+#'
+#' annoFuse is an R package for ...
+#' 
+#' @importFrom dplyr arrange bind_cols case_when count desc distinct filter
+#' group_by inner_join left_join mutate n rename select summarise ungroup %>%
+#' @importFrom ggplot2 aes coord_flip element_blank element_line element_rect
+#' element_text facet_wrap geom_bar geom_col geom_hline geom_line
+#' geom_linerange geom_point geom_segment ggplot ggtitle guides labs
+#' rel scale_colour_manual scale_fill_manual scale_size_continuous
+#' scale_x_discrete scale_y_continuous stat theme theme_void xlab ylab .data
+#' @importFrom ggpubr ggarrange ggexport rotate
+#' @importFrom ggthemes theme_foundation
+#' @importFrom grid arrow unit
+#' @importFrom qdapRegex rm_between
+#' @importFrom readr col_character cols read_tsv
+#' @importFrom reshape2 colsplit dcast melt
+#' @importFrom stats na.omit
+#' @importFrom stringr str_detect str_replace
+#' @importFrom tibble add_column column_to_rownames remove_rownames
+#' @importFrom tidyr gather separate unnest
+#' @importFrom utils head read.delim
+#'
+#' @name annoFuse-pkg
+#' @docType package
+NULL
