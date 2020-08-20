857676568fdd3550daf5468cfb9aa3996afe09ce
Gaonkar <ksgaonkar@outlook.com>
2020-08-20 11:35:58 -0400

    removing non essential columns

--- R/annoFuse-utils.R
+++ R/annoFuse-utils.R
@@ -4,11 +4,10 @@
 .check_annoFuse_calls <- function(standardFusioncalls) {
   stopifnot(is(standardFusioncalls, "data.frame"))
   
-  cols_fusioncalls <- c("Sample", "LeftBreakpoint", "RightBreakpoint", "FusionName", "Caller",
-                        "Fusion_Type", "JunctionReadCount", "SpanningFragCount", "Confidence", 
-                        "annots", "Gene1A", "Gene2A", "Gene1B", "Gene2B", 
-                        "Gene1A_anno", "Gene1B_anno", "Gene2A_anno", "Gene2B_anno",
-                        "Fusion_anno")
+  cols_fusioncalls <- c("LeftBreakpoint", "RightBreakpoint", "FusionName",
+                        "Fusion_Type", "JunctionReadCount", "SpanningFragCount",
+                        "Gene1A", "Gene1B", "Gene1A_anno", "Gene1B_anno",
+                        "Gene2A_anno", "Gene2B_anno","Fusion_anno")
   # TODO: are these all the columns that are specified?
   
   stopifnot(all(cols_fusioncalls %in% colnames(standardFusioncalls)))
