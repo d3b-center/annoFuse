dda0f41fff41b647ab3c97ba336103e633986491
Federico Marini <marinif@uni-mainz.de>
2020-09-04 08:04:19 +0200

    Update R/annoFuse-utils.R
    Co-authored-by: Jo Lynne Rokita <jolynnerokita@d3b.center>

--- R/annoFuse-utils.R
+++ R/annoFuse-utils.R
@@ -5,7 +5,7 @@
   stopifnot(is(standardFusioncalls, "data.frame"))
   
   # with a minimal set of columns to be there...
-  cols_fusioncalls <- c("LeftBreakpoint", "RightBreakpoint", "FusionName",
+  cols_fusioncalls <- c("Sample", "LeftBreakpoint", "RightBreakpoint", "FusionName",
                         "Gene1A", "Gene1B")
   
   stopifnot(all(cols_fusioncalls %in% colnames(standardFusioncalls)))
