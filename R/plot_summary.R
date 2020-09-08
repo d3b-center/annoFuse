a8dc882cfe2d1cede47d5ffc2714ce849c25f36d
Gaonkar <ksgaonkar@outlook.com>
2020-09-08 16:47:47 -0400

    base_size added for flexible user input

--- R/plot_summary.R
+++ R/plot_summary.R
@@ -4,6 +4,7 @@
 #' @param groupby column name with grouping variables
 #' @param outputpdffile Filename to plot image - optional, if not specified, the 
 #' plot is simply generated
+#' @param base_size size for text in plots, defalt 20 for pdf outputs
 #'
 #' @export
 #'
@@ -15,7 +16,8 @@
 #' plot_summary(sfc, groupby = "Fusion_Type")
 plot_summary <- function(standardFusioncalls,
                          groupby,
-                         outputpdffile) {
+                         outputpdffile,
+                         base_size=20) {
   standardFusioncalls <- .check_annoFuse_calls(standardFusioncalls)
   if (!missing(outputpdffile))
     stopifnot(is.character(outputpdffile))
@@ -50,7 +52,7 @@ plot_summary <- function(standardFusioncalls,
 
   p1 <- ggplot(fusion_chrom, aes(x = !!as.name(groupby), fill = fusion_chrom$Distance, alpha = 0.75)) +
     geom_bar() +
-    theme_publication() +
+    theme_publication(base_size = base_size) +
     theme(legend.position = "top") +
     xlab(as.name(groupby)) +
     ylab("Count") +
@@ -64,10 +66,10 @@ plot_summary <- function(standardFusioncalls,
     rotate() +
     xlab("Caller") +
     ylab("Count (log2)") +
-    theme_publication() +
+    theme_publication(base_size = base_size) +
     theme(legend.position = "top") +
     guides(alpha = FALSE) +
-    theme(axis.text.x = element_text(angle = 0, hjust = 1, size = 12), axis.text.y = element_text(angle = 0, vjust = 2, size = 12)) +
+    theme(axis.text.x = element_text(angle = 0, hjust = 1), axis.text.y = element_text(angle = 0, vjust = 2)) +
     labs(fill = "Frame")
 
 
@@ -114,8 +116,8 @@ plot_summary <- function(standardFusioncalls,
     scale_size_continuous(range = c(1, 12)) +
     xlab("Gene Biotype") +
     ylab("Gene Position") +
-    theme_publication() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12), axis.text.y = element_text(angle = 0, vjust = 2, size = 12), legend.text = element_text(size = 12), legend.position = "left") +
+    theme_publication(base_size = base_size) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text.y = element_text(angle = 0, vjust = 1), legend.position = "left") +
     guides(fill = FALSE)
 
 
@@ -150,11 +152,11 @@ plot_summary <- function(standardFusioncalls,
 
     p4 <- ggplot(kinase_fusion, aes(x = Domain, y = Type.ct, fill = gene_position, alpha = 0.95)) +
       geom_col() +
-      theme_publication() +
+      theme_publication(base_size = base_size) +
       ylab("Count") +
       xlab("Kinase domain retained") +
       guides(alpha = FALSE) +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12), axis.text.y = element_text(angle = 0, vjust = 2, size = 12)) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text.y = element_text(angle = 0, vjust = 2)) +
       labs(fill = "Gene Position")
   } else {
     p4 <- ggplot() +
@@ -190,29 +192,33 @@ plot_summary <- function(standardFusioncalls,
   fusion_type_bubblegene_df$gene_position[grep("Gene1B", fusion_type_bubblegene_df$gene_position)] <- "3'-Gene"
   fusion_type_bubblegene_df$gene_position <- factor(fusion_type_bubblegene_df$gene_position, levels = c("5'-Gene", "3'-Gene"), ordered = TRUE)
 
-  p5 <- ggplot(fusion_type_bubblegene_df, aes(x = !!as.name(groupby), y = Annot.ct, size = Annot.ct, fill = Annotation, color = Annotation, alpha = 0.75, width = 5, height = 5)) +
+  p5 <- ggplot(fusion_type_bubblegene_df, aes(x = !!as.name(groupby), y = Annot.ct, size = Annot.ct, fill = Annotation, color = Annotation, alpha = 0.75)) +
     geom_point(shape = 21) +
     guides(size = FALSE, alpha = FALSE) +
     scale_size_continuous(range = c(1, 12)) +
     ylab("Count") +
     xlab(as.name(groupby)) +
-    theme_publication(base_size = 12) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12), axis.text.y = element_text(angle = 0, vjust = 2, size = 12), legend.text = element_text(size = 12), legend.position = "left") +
-    theme(plot.margin = unit(c(0, 10, 0, 15), "mm")) +
+    theme_publication(base_size = base_size) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text.y = element_text(angle = 0, vjust = 2), legend.position = "left") +
     facet_wrap(~gene_position)
 
 
   if (!missing(outputpdffile)) {
-    ggarrange(p1, p2, p3, p4, p5, labels = c("A", "B", "C", "D", "E"), heights = c(5, 5, 6), widths = c(2, 1), nrow = 3, ncol = 2, font.label = list(size = 30)) %>% ggexport(filename = outputpdffile, width = 20, height = 20)
+    ggarrange(p1, p2, p3, p4, p5, 
+              labels = c("A", "B", "C", "D", "E"), 
+              heights = c(5, 4, 6),
+              widths = c(2, 1),
+              nrow = 3, ncol = 2,
+              font.label = list(size = base_size )) %>% ggexport(filename = outputpdffile, width = 20, height = 20)
   } else {
     p1 <- ggplot(fusion_chrom, aes(x = !!as.name(groupby), fill = fusion_chrom$Distance, alpha = 0.75)) +
       geom_bar() +
-      theme_publication(base_size = 24) +
+      theme_publication(base_size = base_size) +
       theme(legend.position = "top") +
       xlab(as.name(groupby)) +
       ylab("Count") +
       guides(alpha = FALSE) +
-      theme(axis.text.x = element_text(angle = 45, size = 6), plot.margin = unit(c(1, 1, 0, 10), "mm")) +
+      theme(axis.text.x = element_text(angle = 45), plot.margin = unit(c(1, 1, 0, 10), "mm")) +
       labs(fill = "Distance")
 
     # frame information
@@ -221,10 +227,10 @@ plot_summary <- function(standardFusioncalls,
       rotate() +
       xlab("Caller") +
       ylab("Count (log2)") +
-      theme_publication(base_size = 24) +
+      theme_publication(base_size = base_size) +
       theme(legend.position = "top") +
       guides(alpha = FALSE) +
-      theme(axis.text.x = element_text(angle = 0, hjust = 1, size = 6), axis.text.y = element_text(angle = 0, vjust = 2, size = 6), plot.margin = unit(c(1, 10, 10, 1), "mm")) +
+      theme(axis.text.x = element_text(angle = 0, hjust = 1), axis.text.y = element_text(angle = 0, vjust = 2), plot.margin = unit(c(1, 10, 10, 1), "mm")) +
       labs(fill = "Frame")
 
     p3 <- ggplot(fusion_type_gene_df, aes(x = gene_biotype, y = gene_position, size = Type.ct, fill = gene_position, color = gene_position, alpha = 0.75, width = 2.5, height = 5)) +
@@ -233,18 +239,18 @@ plot_summary <- function(standardFusioncalls,
       scale_size_continuous(range = c(0.1, 3)) +
       xlab("Gene Biotype") +
       ylab("Gene Position") +
-      theme_publication(base_size = 24) +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 6), axis.text.y = element_text(angle = 0, vjust = 2, size = 6), legend.text = element_text(size = 6), legend.position = "left", plot.margin = unit(c(1, 1, 1, 10), "mm")) +
+      theme_publication(base_size = base_size) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, ), axis.text.y = element_text(angle = 0, vjust = 1), legend.position = "left", plot.margin = unit(c(1, 1, 1, 10), "mm")) +
       guides(fill = FALSE)
 
     if (!is_empty(fusion_kinase_protein_df$GeneSymbol)) {
       p4 <- ggplot(kinase_fusion, aes(x = Domain, y = Type.ct, fill = gene_position, alpha = 0.95)) +
         geom_col() +
-        theme_publication(base_size = 24) +
+        theme_publication(base_size = base_size) +
         ylab("Count") +
         xlab("Kinase domain retained") +
         guides(alpha = FALSE) +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 6), axis.text.y = element_text(angle = 0, vjust = 2, size = 6), plot.margin = unit(c(1, 1, 1, 1), "mm")) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text.y = element_text(angle = 0, vjust = 2), plot.margin = unit(c(1, 1, 1, 1), "mm")) +
         labs(fill = "Gene Position")
     } else {
       p4 <- ggplot() +
@@ -257,17 +263,17 @@ plot_summary <- function(standardFusioncalls,
       scale_size_continuous(range = c(0.1, 3)) +
       ylab("Count") +
       xlab(as.name(groupby)) +
-      theme_publication(base_size = 24) +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 6), axis.text.y = element_text(angle = 0, vjust = 2, size = 6), legend.text = element_text(size = 6), legend.position = "left") +
+      theme_publication(base_size = base_size) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text.y = element_text(angle = 0, vjust = 2), legend.position = "left") +
       theme(plot.margin = unit(c(1, 1, 1, 10), "mm")) +
       facet_wrap(~gene_position)
 
     p <- ggarrange(p1, p2, p3, p4, p5, 
                    labels = c("A", "B", "C", "D", "E"), 
-                   heights = c(3, 2, 3), 
+                   heights = c(5, 3, 5), 
                    widths = c(2, 1), 
                    nrow = 3, ncol = 2, 
-                   font.label = list(size = 12))
+                   font.label = list(size = base_size))
 
     return(p)
   }
