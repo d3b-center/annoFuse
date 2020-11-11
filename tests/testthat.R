29aec7807f87e9753fcfe3fc1ee5b3753ef183ff
Federico Marini <marinif@uni-mainz.de>
2020-11-11 12:44:04 +0100

    Initial backbone for unit testing

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(annoFuse)
+
+test_check("annoFuse")
---
+++ tests/testthat/setup_annoFuse.R
@@ -0,0 +1,7 @@
+# operations that will be used by more tests
+
+## eg long running stuff, loading data, ...
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
