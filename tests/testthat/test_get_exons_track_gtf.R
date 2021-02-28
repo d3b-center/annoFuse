3ad7f2a285feac6c5f1d7edfe0d69d85cdd38120
Krutika Gaonkar <ksgaonkar@outlook.com>
2021-02-27 20:07:21 -0500

    adding test for exons

--- tests/testthat/test_get_exons_track_gtf.R
+++ tests/testthat/test_get_exons_track_gtf.R
@@ -1,10 +1,10 @@
-context("Test biomart pfam data")
+context("Test exons dataframe generation")
 
 library(EnsDb.Hsapiens.v86)
 download.file("ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_27/gencode.v27.primary_assembly.annotation.gtf.gz","gencode.v27.primary_assembly.annotation.gtf.gz")
 
 exons <- get_exons_tracks_gtf(gtf_path = file.path("gencode.v27.primary_assembly.annotation.gtf.gz"))
 
-test_that("Generates pfam biomart data", {
+test_that("Generates exons dataframe", {
   expect_equal(colnames(exons), c( "geneID","contig","strand", "start" ,"end","exonNumber", "transcript", "type", "geneName"  ))
 })
