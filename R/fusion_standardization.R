c73c1c74d6aa32505117d5e5a30aee13db3dff19
Phul <phuls@f792h5qdp6.mynetworksettings.com>
2022-09-12 18:49:23 -0400

    First commit to add custom type annotations

--- R/fusion_standardization.R
+++ R/fusion_standardization.R
@@ -7,7 +7,9 @@
 #' @param fusion_calls A dataframe from star fusion or arriba (more callers to be added)
 #' @param caller string options STARFUSION/ARRIBA
 #' @param tumorID string or character vector of same length as fusion_calls
+#' @param input_json_file (optional) json format config file to provide input and output columns headers required for CUSTOM type and not required for other callers
 #'
+#' @developer Krutika S Gaonkar, Saksham Phul(phuls@chop.edu)
 #' @export
 #'
 #' @return Standardized fusion calls ready for filtering
@@ -29,15 +31,37 @@
 #'   caller = "STARFUSION",
 #'   tumorID = "tumorID"
 #' )
+#  read in CUSTOM type file
+#' formattedCUSTOM <- fusion_standardization(fusionfileStarFusion,
+#'   caller = "CUSTOM",
+#'   tumorID = "All",
+#'   input_json_file = "config"
+#' )
+#' format of the input_json_file ("Input_header" : "Output_header")
+#  {
+#  "CUSTOM":{
+#	  	"Sample": "Sample_output",	
+#		  "FusionName": "FusionName_output",
+#		  "Gene1A": "Gene1A_output",
+#		  "Gene1B": "Gene1B_output",
+#		  "Gene2A": "Gene2A_output",
+#		  "Gene2B": "Gene2B_output",
+#		  "Fusion_Type":"Fusion_Type_output",
+#		  "annots":"annots_output"
+#	    } 
+#  }
+
 fusion_standardization <- function(fusion_calls,
-                                   caller = c("STARFUSION", "ARRIBA"),
-                                   tumorID = "tumorID") {
+                                   caller = c("STARFUSION", "ARRIBA","CUSTOM"),
+                                   tumorID = "tumorID",
+                                  input_json_file = "No file exists")
+{
   stopifnot(is(fusion_calls, "data.frame"))
   stopifnot(is.character(caller))
   stopifnot(is.character(tumorID))
 
   # caller <- match.arg(caller, choices = c("STARFUSION", "ARRIBA"))
-
+ #print(input_json_file)
   if (caller == "STARFUSION") {
     if (!any(grepl("PROT_FUSION_TYPE", colnames(fusion_calls)))) {
       # add NA if --examine_coding_effect was not used while running starfusion
@@ -67,6 +91,8 @@ fusion_standardization <- function(fusion_calls,
         Sample = tumorID,
         Caller = "STARFUSION"
       )
+       formatted_data=shape_output(fusion_calls)
+   return(formatted_data)
   }
   else if (caller == "ARRIBA") {
     if (!any(colnames(fusion_calls) == "annots")) {
@@ -101,10 +127,104 @@ fusion_standardization <- function(fusion_calls,
         Sample = tumorID,
         Caller = "ARRIBA"
       )
-  } else {
+      formatted_data=shape_output(fusion_calls)
+   return(formatted_data)
+  }
+  else if (caller == "CUSTOM") 
+  {
+        #Condition to check if required columns exists in the input and config file not provided by the user
+        if (!file.exists(input_json_file) &&
+            any(colnames(fusion_calls) == "Sample") &&
+            any(colnames(fusion_calls) == "FusionName") &&
+            any(colnames(fusion_calls) == "Gene1A") &&
+            any(colnames(fusion_calls) == "Gene1B") &&
+            any(colnames(fusion_calls) == "Gene2A") &&
+            any(colnames(fusion_calls) == "Gene2B") &&
+            any(colnames(fusion_calls) == "Fusion_Type") &&
+            any(colnames(fusion_calls) == "annots"))
+          {
+            print("All required columns exists! ")
+            return(fusion_calls)
+          }
+      else if(file.exists(input_json_file)) # if config file is provided
+        { 
+          print("Annotating based on config file") 
+      
+          json_data_frame <- as.data.frame(rjson::fromJSON(file = input_json_file)) #Get data from json file
+          json_cols <- colnames(json_data_frame) # extract cols from json data frame
+          caller <- strsplit(json_cols, split = "[.]")[[1]][1] #Get the caller from json file
+  
+          input_columns <- list() #define list to store input columns from json file
+          output_columns <- list() #define list to store output columns from json file
+          for(i in json_cols){
+            input_columns <- append(input_columns,strsplit(i, split = "[.]")[[1]][2])  #extract input columns
+          }
+          
+          if ("Sample" %in% input_columns &&
+            "FusionName" %in% input_columns &&
+            "Gene1A" %in% input_columns &&
+            "Gene1B" %in% input_columns  &&
+            "Gene2A" %in% input_columns &&
+            "Gene2B" %in% input_columns &&
+            "Fusion_Type" %in% input_columns &&
+            "annots" %in% input_columns) #check if required column exists in config file else throw an exception
+            {
+            print("All required columns exists!")   
+            for(i in 1:ncol(json_data_frame)) {       # for-loop over columns
+              output_columns <- append(output_columns,json_data_frame[ , i])
+            }
+           # json_dataframe <- do.call(rbind, Map(data.frame, input_name=input_columns, output_name=output_columns))
+          
+            input_columns_without_None=input_columns 
+            output_columns_without_None=output_columns
+            output_columns_with_None <- list()
+            index_none <-list() 
+
+            for(i in 1:length(input_columns)){ #loop to extract None from input json
+                if(input_columns[i] == "None"){
+                  output_columns_with_None <-append(output_columns_with_None,output_columns[i])
+                  index_none <- append(index_none,i)
+                }
+              }
+            
+            if(length(index_none > 0)){ #check if none exist as an input in the config file
+                input_columns_without_None <- input_columns[- unlist(index_none)]
+                output_columns_without_None <- output_columns[- unlist(index_none)]
+            }
+          
+            standard_calls <- fusion_calls %>% dplyr::select(unlist(input_columns_without_None))
+            standard_calls <- gdata::rename.vars(standard_calls, from = unlist(input_columns_without_None), to = unlist(output_columns_without_None))
+            for(i in output_columns_with_None)
+            {
+                if( i != "Caller"){ #set caller for all the rows if json has None::Caller
+                standard_calls[i] <- NA
+                }
+                else{
+                    standard_calls[i] <- caller
+                }
+            }
+            standard_calls <- standard_calls[, unlist(output_columns)]
+            #print(standard_calls)
+            return(standard_calls)
+          }
+          else{
+            stop(paste("Provide all the required columns in the config file. Required columns for Custom caller are: Sample FusionName Gene1A Gene1B Gene2A Gene2B Fusion_Type annots."))
+          }
+        }  
+       else #if user do not meet input expectations 
+       {
+        stop(paste(caller, " caller requires specific columns or config file do not exists. Required columns are: Sample FusionName Gene1A Gene1B Gene2A Gene2B Fusion_Type annots."))
+       } 
+    } 
+  
+ else 
+ {
     stop(paste(caller, "is not a supported caller string."))
   }
+}  
 
+#function used by STARFUSION and ARRIBA to shape the final output columns as required
+shape_output <- function(fusion_calls){
   # Get standard columns for filtering
 
   standard_calls <- fusion_calls %>%
@@ -141,5 +261,5 @@ fusion_standardization <- function(fusion_calls,
     ) %>%
     as.data.frame()
 
-  return(standard_calls)
+  return(standard_calls)  
 }
--- tests/testthat/test-fusion_standardization.R
+++ tests/testthat/test-fusion_standardization.R
@@ -31,9 +31,22 @@ test_that("Standardizing starfusion calls (with --examine_coding_effect) calls",
   expect_equal(unique(standardFusioncalls$Sample), "BS_W97QQYKQ")
 })
 
+customtypefile <- read.delim(system.file("extdata", "custom_type_example.tsv", package = "annoFuseData"), check.names = FALSE)
+configcustomtypepath <- system.file("extdata", "config", package = "annoFuseData")
+CustomFusioncalls <-
+  annoFuse::fusion_standardization(
+    customtypefile,
+    "CUSTOM",
+    "random",
+    input_json_file=configcustomtypepath
+  )
+
+test_that("Standardizing Custom type calls with config file", {
+  expect_equal(colnames(CustomFusioncalls), c("Sample", "FusionName", "Gene1A", "Gene1B", "Gene2A", "Gene2B", "Fusion_Type", "annots"))
+  expect_equal(nrow(CustomFusioncalls), 283)
+})
 
 fusionfileStarfusion <- read.delim(system.file("extdata", "starfusion_example.tsv", package = "annoFuseData"), check.names = FALSE)
-
 fusionfileStarfusion <- fusionfileStarfusion[, 1:15]
 standardFusioncalls <-
   annoFuse::fusion_standardization(
@@ -41,7 +54,6 @@ standardFusioncalls <-
     caller = "STARFUSION",
     tumorID = "BS_W97QQYKQ"
   )
-
 
 test_that("Standardizing starfusion calls (without --examine_coding_effect) calls", {
   expect_equal(colnames(standardFusioncalls), c("LeftBreakpoint", "RightBreakpoint", "FusionName", "Sample", "Caller", "Fusion_Type", "JunctionReadCount", "SpanningFragCount", "Confidence", "annots", "GeneA", "Gene1A", "Gene2A", "GeneB", "Gene1B", "Gene2B", "BreakpointLocation", "SpanningDelta"))
