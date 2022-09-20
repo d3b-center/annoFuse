f102c343ee200592d94007815065af1d23c63965
Federico Marini <marinif@uni-mainz.de>
2022-09-20 12:17:58 +0200

    introducing some better indentation in the test suite

--- tests/testthat/test-fusion_standardization.R
+++ tests/testthat/test-fusion_standardization.R
@@ -1,6 +1,7 @@
 context("Test fusion standardization")
 
-fusionfileArriba <- read.delim(system.file("extdata", "arriba_example.tsv", package = "annoFuseData"), check.names = FALSE)
+fusionfileArriba <- read.delim(
+  system.file("extdata", "arriba_example.tsv", package = "annoFuseData"), check.names = FALSE)
 
 standardFusioncalls <-
   annoFuse::fusion_standardization(
@@ -10,13 +11,19 @@ standardFusioncalls <-
   )
 
 test_that("Standardizing arriba calls", {
-  expect_equal(colnames(standardFusioncalls), c("LeftBreakpoint", "RightBreakpoint", "FusionName", "Sample", "Caller", "Fusion_Type", "JunctionReadCount", "SpanningFragCount", "Confidence", "annots", "GeneA", "Gene1A", "Gene2A", "GeneB", "Gene1B", "Gene2B", "BreakpointLocation", "SpanningDelta"))
+  expect_equal(colnames(standardFusioncalls), 
+               c("LeftBreakpoint", "RightBreakpoint", "FusionName", 
+                 "Sample", "Caller", "Fusion_Type", "JunctionReadCount", 
+                 "SpanningFragCount", "Confidence", "annots", 
+                 "GeneA", "Gene1A", "Gene2A", "GeneB", "Gene1B", "Gene2B", 
+                 "BreakpointLocation", "SpanningDelta"))
   expect_equal(nrow(standardFusioncalls), 25)
   expect_equal(unique(standardFusioncalls$Sample), "BS_W97QQYKQ")
 })
 
 
-fusionfileStarfusion <- read.delim(system.file("extdata", "starfusion_example.tsv", package = "annoFuseData"), check.names = FALSE)
+fusionfileStarfusion <- read.delim(
+  system.file("extdata", "starfusion_example.tsv", package = "annoFuseData"), check.names = FALSE)
 
 standardFusioncalls <-
   annoFuse::fusion_standardization(
@@ -26,13 +33,19 @@ standardFusioncalls <-
   )
 
 test_that("Standardizing starfusion calls (with --examine_coding_effect) calls", {
-  expect_equal(colnames(standardFusioncalls), c("LeftBreakpoint", "RightBreakpoint", "FusionName", "Sample", "Caller", "Fusion_Type", "JunctionReadCount", "SpanningFragCount", "Confidence", "annots", "GeneA", "Gene1A", "Gene2A", "GeneB", "Gene1B", "Gene2B", "BreakpointLocation", "SpanningDelta"))
+  expect_equal(colnames(standardFusioncalls), 
+               c("LeftBreakpoint", "RightBreakpoint", "FusionName", 
+                 "Sample", "Caller", "Fusion_Type", "JunctionReadCount", "SpanningFragCount", 
+                 "Confidence", "annots", 
+                 "GeneA", "Gene1A", "Gene2A", "GeneB", "Gene1B", "Gene2B", 
+                 "BreakpointLocation", "SpanningDelta"))
   expect_equal(nrow(standardFusioncalls), 1)
   expect_equal(unique(standardFusioncalls$Sample), "BS_W97QQYKQ")
 })
 
 
-fusionfileStarfusion <- read.delim(system.file("extdata", "starfusion_example.tsv", package = "annoFuseData"), check.names = FALSE)
+fusionfileStarfusion <- read.delim(
+  system.file("extdata", "starfusion_example.tsv", package = "annoFuseData"), check.names = FALSE)
 fusionfileStarfusion <- fusionfileStarfusion[, 1:15]
 standardFusioncalls <-
   annoFuse::fusion_standardization(
@@ -42,12 +55,18 @@ standardFusioncalls <-
   )
 
 test_that("Standardizing starfusion calls (without --examine_coding_effect) calls", {
-  expect_equal(colnames(standardFusioncalls), c("LeftBreakpoint", "RightBreakpoint", "FusionName", "Sample", "Caller", "Fusion_Type", "JunctionReadCount", "SpanningFragCount", "Confidence", "annots", "GeneA", "Gene1A", "Gene2A", "GeneB", "Gene1B", "Gene2B", "BreakpointLocation", "SpanningDelta"))
+  expect_equal(colnames(standardFusioncalls), 
+               c("LeftBreakpoint", "RightBreakpoint", "FusionName", 
+                 "Sample", "Caller", "Fusion_Type", "JunctionReadCount", "SpanningFragCount", 
+                 "Confidence", "annots", 
+                 "GeneA", "Gene1A", "Gene2A", "GeneB", "Gene1B", "Gene2B", 
+                 "BreakpointLocation", "SpanningDelta"))
   expect_equal(nrow(standardFusioncalls), 1)
   expect_equal(unique(standardFusioncalls$Sample), "BS_W97QQYKQ")
 })
 
-customtypefile <- read.delim(system.file("extdata", "custom_type_example.tsv", package = "annoFuseData"), check.names = FALSE)
+customtypefile <- read.delim(
+  system.file("extdata", "custom_type_example.tsv", package = "annoFuseData"), check.names = FALSE)
 configcustomtypepath <- system.file("extdata", "config", package = "annoFuseData")
 CustomFusioncalls <-
   annoFuse::fusion_standardization(
@@ -58,6 +77,8 @@ CustomFusioncalls <-
   )
 
 test_that("Standardizing Custom type calls with config file", {
-  expect_equal(colnames(CustomFusioncalls), c("Sample", "FusionName", "Gene1A", "Gene1B", "Gene2A", "Gene2B", "Fusion_Type", "annots"))
+  expect_equal(colnames(CustomFusioncalls), 
+               c("Sample", "FusionName", "Gene1A", "Gene1B", "Gene2A", "Gene2B", 
+                 "Fusion_Type", "annots"))
   expect_equal(nrow(CustomFusioncalls), 283)
 })
